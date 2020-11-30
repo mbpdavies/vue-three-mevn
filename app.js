76b5bb32589c334d7df8848420175f157b81009d
Myles Davies <mbpdavies@gmail.com>
2020-11-30 20:36:14 +0000

    second commit

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+// load .env file
+require("dotenv").config();
+// get PORT from env file
+const port = process.env.PORT;
 
 // connect to mongodb
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  "mongodb+srv://vuethree:vuethree@cluster0.xo538.mongodb.net/MEVN-app?retryWrites=true&w=majority",
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xo538.mongodb.net/MEVN-app?retryWrites=true&w=majority`,
   { useNewUrlParser: true, useUnifiedTopology: true },
   (err) => {
     if (err) {
